7f8838b87244d0227651c30854b8fe9aec6101f0
Maxim <maxkryu2004@gmail.com>
2024-08-10 03:18:46 +0300

    init

---
+++ script/modal.js
@@ -0,0 +1,14 @@
+let modalCreateid = document.getElementById("modal__Container_create")
+let modalid = document.getElementById("modal__Container")
+function openModalCreate(){
+    modalCreateid.style.display = "flex"
+}
+function openModal(){
+    modalid.style.display= 'flex'
+}
+function closeModal(){
+    modalid.style.display = "none"
+}
+function closeCreateModal(){
+    modalCreateid.style.display = "none"
+}
